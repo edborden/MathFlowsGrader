59a109f9631c36bc7ac20df1b9e55cdd8b973408
edborden <borden.edward@gmail.com>
2015-11-06 11:40:30 -0500

    font-awesome

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,11 +6,19 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  var pickFiles = require('broccoli-funnel');
+  var mergeTrees = require('broccoli-merge-trees');
+
   // Swing
   app.import('bower_components/swing/dist/browser/swing.js');
 
   // Drawing
   app.import('bower_components/signature_pad/signature_pad.js');
 
-  return app.toTree();
+  // Font-Awesome
+  var fontAwesomeFonts = pickFiles('bower_components/components-font-awesome/fonts', {
+    destDir: '/fonts'
+  });
+
+  return mergeTrees([app.toTree(),fontAwesomeFonts]);
 };
